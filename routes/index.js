32cc0a99d829cf403731f5bcce7bd6d1fbf21fbd
Ivan <latimmir@gmail.com>
2020-03-15 12:46:08 +0200

    login functionality

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
-router.get("/", function (req, res) {
+router.get("/", function (req, res){
   res.render('index');
+});
+
+router.get("/index", function(req,res){
+  res.redirect('/');
+});
+
+router.get("/login", function(req, res){
+  res.render('login');
 });
 
 // MUST HAVE LINE in case you want it working =)
