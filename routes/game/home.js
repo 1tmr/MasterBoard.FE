6aa9a4470d89d74d5365e839a792cb6c0698693c
Ivan <latimmir@gmail.com>
2020-03-22 19:40:16 +0200

    Game UIs design (1st deck)

--- routes/game/home.js
+++ routes/game/home.js
@@ -4,4 +4,13 @@ router.get('/', (req, res) => {
   res.render('home/selector', {title: "Game Board News", _path: "/game"});
 });
 
+router.get('/:gid', (req, res) =>{
+  if(req.params.gid == 2){
+    var game = {title: "Harry P"}
+    res.render('home/game/feed', game);
+  } else {
+    res.render('404_error');
+  }
+});
+
 module.exports = router;
